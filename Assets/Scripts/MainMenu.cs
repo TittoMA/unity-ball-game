3dd2fa156028dc884636a3b933b11cdb31202a3d
rama <untitled99099@gmail.com>
2022-10-10 18:19:01 +0700

    update kedua
    tambahan main menu + isi

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+//using System.Net.Mime;
+//using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  public void PlayGame(){
+    SceneManager.LoadScene("SampleScene");
+  }
+  public void QuitGame(){
+    Debug.Log("QUIT!");
+    Application.Quit();
+  }
+}
