42b946c02df088c9838243b48d4c460e6370c677
TittoMA <tittomahogany@gmail.com>
2022-10-16 22:11:16 +0700

    fixing script

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -8,25 +8,33 @@ public class BallMovement : MonoBehaviour
     
     public float speed = 2.0f;
     public float jumpForce = 10.0f;
-    private Rigidbody rb;
-    bool isGrounded;
-
     public Camera cam;
     public float dirRotate = -27.0f;
 
+    private Rigidbody rb;
+    private Animator animator;
+
+    bool isGrounded;
+    bool prevGrounded = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isGrounded && !prevGrounded){
+            animator.SetTrigger("TgSquish");
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animator.SetTrigger("TgStretch");
         }
     }
 
@@ -54,9 +62,12 @@ public class BallMovement : MonoBehaviour
         }
     }
 
+    void LateUpdate(){
+        prevGrounded = isGrounded;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-    
         if(collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
